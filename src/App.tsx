4773b5fdc3e1957224d9dbbb848c887f30329768
Nikunj Goswami <shreyaenterprises562@gmail.com>
2026-03-13 15:43:03 +0530

    fix legal pages routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,9 @@ import CategoryPage from './pages/CategoryPage';
 import BlogPage from './pages/BlogPage';
 import BlogPostPage from './pages/BlogPostPage';
 import SubmitTool from './pages/SubmitTool';
-import SEOPage from './pages/SEOPage';
 import FoundersPage from './pages/FoundersPage';
+import SEOPage from './pages/SEOPage';
 
-/* NEW PAGES */
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import PrivacyPage from "./pages/PrivacyPage";
@@ -25,7 +24,6 @@ function App() {
     <HelmetProvider>
       <Router>
 
-        {/* Scroll to top on route change */}
         <ScrollToTop />
 
         <div className="flex min-h-screen flex-col font-sans text-slate-900 antialiased">
@@ -46,13 +44,13 @@ function App() {
               <Route path="/tool/:slug" element={<ToolPage />} />
               <Route path="/category/:category" element={<CategoryPage />} />
 
-              {/* Legal / info pages */}
+              {/* Legal pages */}
               <Route path="/about" element={<AboutPage />} />
               <Route path="/contact" element={<ContactPage />} />
               <Route path="/privacy" element={<PrivacyPage />} />
               <Route path="/terms" element={<TermsPage />} />
 
-              {/* SEO dynamic pages */}
+              {/* SEO dynamic pages (always last) */}
               <Route path="/:keyword" element={<SEOPage />} />
 
             </Routes>
